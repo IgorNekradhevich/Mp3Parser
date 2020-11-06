e4d0ba52f7825bd59c595650148a6f197ba05138
Игорь Некрашевич <oikmaster@yandex.ru>
2020-11-06 19:27:15 +0700

    Mp3 v0.1

---
+++ WpfApp8/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp8
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            Parsing parsing = new Parsing();
+            if (e.Key == Key.Enter)
+            {
+                ListName.Items.Clear();
+                parsing.getSite(NameSound.Text);
+                List<string> result = parsing.generationList();
+                foreach (string x in result)
+                {
+                    ListName.Items.Add(x);
+                }
+            }
+            
+                //MessageBox.Show(parsing.getSite(NameSound.Text));
+        }
+    }
+}
---
+++ WpfApp8/Parsing.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp8
+{
+    class Parsing
+    {
+        private WebResponse response;
+        private WebRequest request;
+        private string site;
+        private string adress = "https://zvooq.pro/artists/";
+        private List<string> name = new List<string>();
+        private List<string> url = new List<string>();
+
+        public string getSite(string nameSound)
+        {
+            request = WebRequest.Create(adress + nameSound);
+            response = request.GetResponse();
+            Stream stream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(stream);
+            site = reader.ReadToEnd();
+            response.Close();
+            return site;
+        }
+
+        public List<string> generationList()
+        {
+            name.Clear();
+            int index = 0;
+            while (index != -1)
+            {
+                index = site.IndexOf("data-title");
+                if (index != -1)
+                {
+                    site = site.Remove(0, index + 12);
+                    name.Add(site.Remove(site.IndexOf("\"")));
+                }
+            }
+            return name;
+        }
+    }
+}
